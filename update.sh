77f8c54bb82f8335b4cd569c955ce928cf287503
xuzhiwei <xuzhiwei@surfin.sg>
2021-02-19 20:19:15 +0800

    rm sh

--- update.sh
+++
@@ -1,5 +0,0 @@
-###
- # @Date: 2021-02-19 19:54:24
- # @LastEditTime: 2021-02-19 20:11:52
-### 
-time=`date +"%Y-%m-%d"` && cat README.md | sed "s/【.*-.*-.*】/【$time】/g" > a && cp a README.md && rm a
